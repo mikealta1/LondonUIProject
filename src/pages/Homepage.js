be174fa661447ab4ee47108106dde0f5ad669dae
Mike Altamirano <mikealta1@gmail.com>
2023-03-08 22:00:06 -0500

    updated homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './css/Homepage.css'
 
 const Homepage = () =>{
     return (
-        <h1>This is the Homepage</h1>
+        <div className='imgHolder'>
+            <div className='welcomeTxt'>Welcome to London</div>
+            <p className='welcomeParagraph'>From going pubbing to walking along the Thames river we hope you enjoy your stay! We have some suggestions for where to stay and where to have fun.</p>
+        </div> 
     );
 };
 
